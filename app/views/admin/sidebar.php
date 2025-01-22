0de0746146c5ac7ee139bfbbf5802c1419aa2136
shashithucsc <shashithrashmika2001@gmail.com>
2025-01-22 22:39:54 +0530

    session haduwa. supplier id eka card eke pennanna haduwa

--- app/views/admin/sidebar.php
+++ app/views/admin/sidebar.php
@@ -1,16 +1,5 @@
 <?php
-
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-if (!isset($_SESSION['user_id'])) {
-    header("Location: " . URLROOT . "/login");
-    exit();
-}
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
